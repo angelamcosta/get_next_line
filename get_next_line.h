7fdf1dc1ba6fa128144c40f4c362639a5c899ef6
Angela Lima <angelamcostalima@icloud.com>
2022-10-16 20:26:16 +0100

    add header and utils files

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 10:24:04 by anlima            #+#    #+#             */
+/*   Updated: 2022/10/16 18:27:49 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# ifndef  BUFFER_SIZE
+#  define  BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strdup(char *src);
+size_t	ft_strlen(char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strchr(const char *s, int c);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/16 10:25:50 by anlima            #+#    #+#             */
+/*   Updated: 2022/10/16 18:51:32 by anlima           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strdup(char *src)
+{
+	char	*copy;
+	int		i;
+
+	copy = malloc(ft_strlen(src) + 1);
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
+}
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	if (len > ft_strlen((char *)s))
+		len = ft_strlen((char *)s);
+	subs = (char *)malloc(len + 1);
+	if (!subs)
+		return (NULL);
+	i = 0;
+	while (s[start] && i < len)
+		subs[i++] = s[start++];
+	subs[i] = '\0';
+	return (subs);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	int		i;
+
+	i = 0;
+	joined = malloc(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
+	if (!joined)
+		return (NULL);
+	while (joined[i] && *s1)
+		joined[i++] = *s1++;
+	while (joined[i] && *s2)
+		joined[i++] = *s2++;
+	joined[i] = '\0';
+	return (joined);
+}
+
+int	ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	if (s[i] != c)
+		return (0);
+	return (i);
+}
