8bcb710112a70e126968c2da031ec4df0b3a9e2e
Angela Lima <angelamcostalima@icloud.com>
2022-11-10 15:22:58 +0000

    :construction: wip -> utils

--- files/get_next_line_utils.c
+++ files/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 10:25:50 by anlima            #+#    #+#             */
-/*   Updated: 2022/11/10 14:59:57 by anlima           ###   ########.fr       */
+/*   Updated: 2022/11/10 15:17:51 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ char	*ft_strjoin(char *s1, char *s2)
 	char	*joined;
 	int		i;
 
-	if (ft_strchr(s2, '\n') > -1)
-		s2 += ((ft_strchr(s2, '\n')) + 1);
 	joined = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!joined)
 		return (0);
@@ -32,9 +30,9 @@ char	*ft_strjoin(char *s1, char *s2)
 	return (joined);
 }
 
-int	ft_strlen(char *s)
+size_t	ft_strlen(char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = -1;
 	if (!s || *s == '\0')
@@ -49,9 +47,9 @@ int	ft_strchr(char *s, char c)
 	int	i;
 
 	i = 0;
-	while (s && s[i] != c)
+	while (s && s[i] && s[i] != c)
 		i++;
-	if (s && s[i] == c)
+	if (s && s[i] && s[i] == c)
 		return (i);
 	return (-1);
 }
