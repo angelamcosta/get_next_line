ecdda1caadad0ba2d50c29cd9eb89cb7095483c3
Angela Lima <angelamcostalima@icloud.com>
2022-11-14 17:52:27 +0000

    :ambulance: hotfix to mandatory and bonus

--- files/get_next_line.c
+++ files/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: anlima <anlima@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/16 10:24:04 by anlima            #+#    #+#             */
-/*   Updated: 2022/11/14 15:25:02 by anlima           ###   ########.fr       */
+/*   Updated: 2022/11/14 17:40:31 by anlima           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,16 @@ char	*get_next_line(int fd)
 	int			i;
 
 	if (fd < 0 || BUFFER_SIZE < 1 || FOPEN_MAX < fd)
-		return (0);
+		return (NULL);
 	next_line = ft_strjoin(0, line);
 	if (ft_clean(line) > 0)
 		return (next_line);
 	i = read(fd, line, BUFFER_SIZE);
+	if (i < 0)
+	{
+		free(next_line);
+		return (NULL);
+	}
 	while (i > 0)
 	{
 		line[i] = '\0';
